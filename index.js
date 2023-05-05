a1cfbefe165ba9437ea583fb81f1f6a55d6adc87
unknown <tashfique.hcn@gmail.com>
2023-05-05 15:20:25 +0600

    code added in index file to get data from json file

--- index.js
+++ index.js
@@ -2,10 +2,16 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
 
+const chefs = require('./data/chefs.json');
+
 app.get('/', (req, res) => {
-  res.send('Recipe Hunter is Running')
+    res.send('Recipe Hunter is Running')
 });
 
+app.get('/chefs', (req, res) => {
+    res.send(chefs);
+})
+
 app.listen(port, () => {
-  console.log(`Recipe Hunter is Running on port: ${port}`)
+    console.log(`Recipe Hunter is Running on port: ${port}`)
 })
